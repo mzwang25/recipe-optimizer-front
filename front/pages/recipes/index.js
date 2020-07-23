5fc869a91475e074643669c506942a66f93c2c26
mzwang25 <mzwang25@gmail.com>
2020-07-23 13:09:59 -0700

    disable zoom on mobile

--- front/pages/recipes/index.js
+++ front/pages/recipes/index.js
@@ -2,10 +2,15 @@ import Recipes from '../../components/Recipes'
 import ArrowBackIcon from '@material-ui/icons/ArrowBack';
 import IconButton from '@material-ui/core/IconButton'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function home() {
     return (
         <div>
+            <Head>
+            <meta meta name="viewport" content= "width=device-width, user-scalable=no" /> 
+
+            </Head>
             <Link href='/'>
                 <IconButton>
                     <ArrowBackIcon/>
